b8fdeeae0000636b19c6700844868cf927701797
Daniel Espinosa <danielespimau@gmail.com>
2025-12-03 14:02:24 +0100

    Quitar comas y poner puntos en el precio

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,11 +2,19 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
+  LOCALE_ID,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+
+// 👇 Importaciones correctas para Angular 16/17
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+// 👇 Registramos el locale una vez
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +22,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+
+    // 👇 Activamos el formato español
+    { provide: LOCALE_ID, useValue: 'es-ES' },
   ],
 };
