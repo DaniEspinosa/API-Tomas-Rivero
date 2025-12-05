af9ab81b5d635cea77840a32cc72beb2eaf3cce1
Daniel Espinosa <danielespimau@gmail.com>
2025-12-05 13:11:18 +0100

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -15,7 +15,7 @@ router.post("/login", async (req, res) => {
   const token = jwt.sign(
     { id: u.id, email: u.email, rol: u.rol },
     process.env.JWT_SECRET,
-    { expiresIn: "10s" }
+    { expiresIn: "8h" }
   );
   res.json({ token });
 });
